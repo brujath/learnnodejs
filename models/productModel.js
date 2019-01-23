91757855bb152a650ab6576d5b6d3d8d1a9a1dc3
brujat <brujat.u@ymedialabs.com>
2019-01-23 18:50:01 +0530

    retriving from a file instead of an direct array

--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,8 @@
-const Products = [];
+const fs = require('fs');
+
+const path = require('path');
+
+//const Products = [];
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +10,30 @@ module.exports = class Product {
     }
 
     addProduct() {
-        Products.push(this);
+        //Products.push(this);
+        const filePath = path.join(path.dirname(process.mainModule.filename), "data", "products.json"); //resolves path on all os's, foldername, filename with extension
+        fs.readFile(filePath, (err, fileContent)=> {
+            let products = [];
+            if(!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this); // instance created while adding- will be a object with title.
+            fs.writeFile(filePath, JSON.stringify(products), (err)=> {
+                console.log(err);
+            });
+        })
     };
 
-    static getProducts() {
-        return Products;
+    //static method are used to call function with out initialization
+    static getProducts(callBack) {
+        const filePath = path.join(path.dirname(process.mainModule.filename), "data", "products.json"); //resolves path on all os's, foldername, filename with extension
+        fs.readFile(filePath, (err, fileContent) => {
+            if(err) {
+                callBack([]);
+            }
+
+            callBack(JSON.parse(fileContent));
+        });
+        //return Products;
     }
 }
